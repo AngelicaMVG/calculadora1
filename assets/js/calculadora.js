530f9ee9ec0bf8daebebbdf78ab13aad6a647399
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-03-28 08:48:56 -0600

    tiene css

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -2,27 +2,33 @@
 function mostrar(num){
   var numero1 = document.calculadora.numero.value;
   document.calculadora.numero.value = numero1 + num;
+
 }
 
 function limpiar(){
-  var borrar = document.getElementById('pantalla')
-  borrar.value = '';
+  var teclaC = document.getElementById('pantalla')
+  teclaC.value = '';
 }
 
-// function operar(valor){
-// 	if (numero1 == 0){
-// 		resultado = parseInt(document.getElementById('pantalla').value);
-// 	}
-// 	numero2 = parseInt(numero1);
-// 	numero1= 0;
-// 	operacion = valor;
+ function operar(){
+   var valor1 = document.getElementById('pantalla').value;
+   var guarda = valor1.split('+');
+   var suma = Number(guarda[0] )+ Number(guarda[1]);
+   document.getElementById('pantalla').value = suma;
+
+
+	}
+
+// function operaciones(valor){
+//   var numeroPresionado = document.getElementById('pantalla');
+//   numeroPresionado.value = eval(numeroPresionado.value);
 // }
-//
-// function esIgual(){
-//   numero1 = parseInt(numero1);
-//   switch (operacion){
-//     case 1: numero1 += numero2;
-//     break;
+// function validarNumero(event){
+//   var ascci = event.keyCode;
+//   if(ascci== 13){
+//     operaciones();
 //   }
-//
+//   if( ascci < 42 || ascci > 57){
+//     return false;
+//   }
 // }
